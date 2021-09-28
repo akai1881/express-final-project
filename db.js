229f578b1300129cf16d2fbe15e5d20e2a604cc2
akai1881 <akaibegaliev1881@gmail.com>
2021-09-28 12:29:24 +0600

    add database url

--- db.js
+++ db.js
@@ -2,10 +2,20 @@ const { Sequelize } = require('sequelize');
 
 const { DB_NAME, DB_USER, DB_HOST, DB_PASSWORD, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    dialect: 'postgres',
-    host: DB_HOST,
-    port: DB_PORT,
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        logging: true, //false
+    });
+} else {
+    sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+        dialect: 'postgres',
+        host: DB_HOST,
+        port: DB_PORT,
+    });
+}
 
 module.exports = sequelize;
